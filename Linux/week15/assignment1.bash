2e18cbf8e944ab506a69b5db9e2b385f2696b943
Michael Olave <michaelolave12@gmail.com>
2025-12-08 18:47:20 -0500

    Refactor assignment1.bash to enhance course display functions and improve user interaction

--- Linux/week15/assignment1.bash
+++ Linux/week15/assignment1.bash
@@ -1,70 +1,61 @@
 #! /bin/bash
 clear
 
-# filling courses.txt
-bash courses.bash
-
+bash Courses.bash
 courseFile="courses.txt"
 
-function displayCoursesofInst(){
-
-echo -n "Please Input an Instructor Full Name: "
-read instName
-
-echo ""
-echo "Courses of $instName :"
-cat "$courseFile" | grep "$instName" | cut -d';' -f1,2 | \
-sed 's/;/ | /g'
-echo ""
-
+function displayCoursesofInst() {
+    echo -n "Please Input an Instructor Full Name: "
+    read instName
+    echo ""
+    echo "Courses of $instName :"
+    awk -F';' -v name="$instName" 'NR>1 && tolower($7) ~ tolower(name) {print $1" | "$2" | "$5" | "$6" | "$7}' "$courseFile"
+    echo ""
 }
 
-function courseCountofInsts(){
-
-echo ""
-echo "Course-Instructor Distribution"
-cat "$courseFile" | cut -d';' -f7 | \
-grep -v "/" | grep -v "\.\.\." | \
-sort -n | uniq -c | sort -n -r 
-echo ""
-
+function courseCountofInsts() {
+    echo ""
+    echo "Course-Instructor Distribution"
+    awk -F';' 'NR>1 && $7 !~ /^[0-9]/ && $7 !~ /AM|PM/ && $7 !~ /\// && $7 != "" {print $7}' "$courseFile" \
+       | sort | uniq -c | sort -nr
+    echo ""
 }
 
-# TODO - 1
-# Make a function that displays all the courses in given location
-# function dislaplays course code, course name, course days, time, instructor
-# Add function to the menu
-# Example input: JOYC 310
-# Example output: See the screenshots in canvas
+function displayCoursesByLocation() {
+    echo -n "Please Input a Location: "
+    read location
+    echo ""
+    echo "Courses in $location :"
+    awk -F';' -v loc="$location" 'NR>1 && $10 == loc {print $1" | "$2" | "$5" | "$6" | "$7}' "$courseFile"
+    echo ""
+}
 
-# TODO - 2
-# Make a function that displays all the courses that has availability
-# (seat number will be more than 0) for the given course code
-# Add function to the menu
-# Example input: SEC
-# Example output: See the screenshots in canvas
+function displayAvailableCourses() {
+    echo -n "Please Input a Course Code: "
+    read courseCode
+    echo ""
+    echo "Available courses for $courseCode :"
+    awk -F';' -v code="$courseCode" 'NR>1 && toupper($1) ~ toupper(code) && $4 > 0 {print $1" | "$2" | "$4 " seats available"}' "$courseFile"
+    echo ""
+}
 
 while :
 do
-	echo ""
-	echo "Please select and option:"
-	echo "[1] Display courses of an instructor"
-	echo "[2] Display course count of instructors"
-	echo "[5] Exit"
-
-	read userInput
-	echo ""
-
-	if [[ "$userInput" == "5" ]]; then
-		echo "Goodbye"
-		break
-
-	elif [[ "$userInput" == "1" ]]; then
-		displayCoursesofInst
-
-	elif [[ "$userInput" == "2" ]]; then
-		courseCountofInsts
-
-	# TODO - 3 Display a message, if an invalid input is given
-	fi
+    echo ""
+    echo "Please select an option:"
+    echo "[1] Display courses of an instructor"
+    echo "[2] Display course count of instructors"
+    echo "[3] Display courses of a classroom"
+    echo "[4] Display available courses of subject"
+    echo "[5] Exit"
+    read userInput
+    echo ""
+    case "$userInput" in
+        1) displayCoursesofInst ;;
+        2) courseCountofInsts ;;
+        3) displayCoursesByLocation ;;
+		4) displayAvailableCourses ;;
+        5) echo "Goodbye"; break ;;
+        *) echo "Invalid input. Please select between 1-5." ;;
+    esac
 done
